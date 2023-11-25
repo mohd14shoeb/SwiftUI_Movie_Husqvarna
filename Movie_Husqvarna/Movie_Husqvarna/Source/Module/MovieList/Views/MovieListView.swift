bc05bc94f93a3e07bd5b1c8e4c1456888ed2d71b
Shoeb Khan <>
2023-11-25 14:48:58 +0530

    Project setup code

---
+++ Movie_Husqvarna/Movie_Husqvarna/Source/Module/MovieList/Views/MovieListView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListView.swift
+//  Movie_Husqvarna
+//
+//  Created by Shoeb Khan on 25/11/23.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieListView()
+}
